375bfa2a41d12e386be18418b8edfc6b7f359509
Rachel Etnire <rachel.etnire@gmail.com>
2016-09-12 20:50:07 -0700

    add additional form elements in jquery such as colors and multiple choice

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,16 @@ $(document).ready(function() {
     var pet2Input = $("input#pet2").val();
     var pet3Input = parseInt($("input#pet3").val());
     var cattype = $("#cattype").val();
+    var bday = $("#birthday").val();
+    var catColor = $("#color").val();
 
     $(".name1").text(name1Input);
     $(".pet1").text(pet1Input);
     $(".pet2").text(pet2Input.toUpperCase());
     $(".pet3").text(pet3Input);
     $(".cattype").text(cattype);
+    $(".birthday").text(bday);
+    $(".color").text(catColor);
 
     $("#response").show();
 
